9b1a2a22218e159b5ccf24f5c339516b6e5833cf
Mark Kasule <mlkasule3@montgomerycollege.edu>
2021-02-08 20:53:26 -0500

    Added newer version

--- Statistics.java
+++ Statistics.java
@@ -1,6 +1,17 @@
+/**
+* Class: CMSC 201
+* Instructor: Ashique Tanveer
+* Description: Project 2: The program prompts the user to enter ten numbers, and then displays the mean and standard deviations of those numbers 
+* Due:02/09/2021
+* I pledge that I have completed the programming assignment independently.
+* I have not copied the code from a student or any source.
+* I have not given my code to any student.
+*Print your Name here:  Mark Kasule
+*/
+
 import java.util.Scanner;
 
-public class StatisticsCalculator {
+public class Statistics {
 
 	public static void main(String[] args) {
 		
@@ -33,27 +44,25 @@ public class StatisticsCalculator {
 			totalSquared += squaredInput;
 			
 		}
-		System.out.println("Total is: " + total + " Sqaured total is: " + totalSquared);
 		
 		//Calculate the Mean, (mean = total / number of counts)
 		mean = total / NUM_OF_COUNTS;
-		System.out.println("Mean is: " + mean);
+		System.out.printf("Mean is: %.3f \n", mean);
 		
 		//squared mean inputs
 		double SquaredInputmean = totalSquared / NUM_OF_COUNTS;
-		System.out.println("Squared mean is: " + SquaredInputmean);
-		
+
 		//Solve equation inside the loop
 		double insideEquation = SquaredInputmean - ((Math.pow(mean, 2)) / NUM_OF_COUNTS);
 		
-		//dive the inside equation
+		//divide the inside equation
 		double wholeEquationDivided = (insideEquation) / (NUM_OF_COUNTS - 1);
 		
 		//solve for standard deviation
 		deviation = Math.pow(wholeEquationDivided, 0.5);
 		
 		//print out all result
-		System.out.println("The standard deviation is " + deviation);
+		System.out.printf("The standard deviation is %.3f \n", deviation);
 		
 
 	}
